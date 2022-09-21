a29a22c707984dc5e872dcbb416314ffcd59e9c4
Alex McG <alex.mcgonagle.dev@gmail.com>
2022-09-21 15:23:14 +0300

    test deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
-
+//deploy
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/DeFi-Holding-Site/",
